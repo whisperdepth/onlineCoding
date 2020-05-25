74e21daf1cb8faf90e5b043c6b6faabcd6b8247d
Roman <whisperdepth@gmail.com>
2020-05-25 11:52:49 +0300

    Add files via upload

---
+++ 25-05/index.js
@@ -0,0 +1,18 @@
+class Sportsman {
+  constructor(name) {
+    this.name = name;
+  }
+  run() {
+    console.log(`${this.name} is running`);
+  }
+}
+
+class Swimmer extends Sportsman {
+  constructor(name, style) {
+    super(name)
+    this.style = style;
+  }
+  swim() {
+    console.log(`${this.name} is swimming ${this.style}`);
+  }
+}
